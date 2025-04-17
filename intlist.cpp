db69c6b83091fe46afd64f0f775e4fa63931d9c4
Kenneth Chen <kennethychen@cs.ucsb.edu>
2025-04-16 23:00:36 -0700

    commit

--- intlist.cpp
+++ intlist.cpp
@@ -1,87 +1,137 @@
-// intlist.cpp
-// Implements class IntList
-// YOUR NAME(S), AND DATE
 
 #include "intlist.h"
 
 #include <iostream>
 using std::cout;
 
+// constructor sets up empty list
+IntList::IntList() {
+    head = nullptr;
+}
+
 // copy constructor
 IntList::IntList(const IntList& source) {
-    //IMPLEMENT THIS
+    head = nullptr;
+    Node* curr = source.head;
+    while (curr) {
+        push_back(curr->info);
+        curr = curr->next;
+    }
 }
 
 // destructor deletes all nodes
 IntList::~IntList() {
-    //IMPLEMENT THIS
+    Node* curr = head;
+    while (curr) {
+        Node* next = curr->next;
+        delete curr;
+        curr = next;
+    }
 }
-
 
 // return sum of values in list
 int IntList::sum() const {
-    return 0; // REPLACE THIS NON-SOLUTION
+    int total = 0;
+    Node* curr = head;
+    while (curr) {
+        total += curr->info;
+        curr = curr->next;
+    }
+    return total;
 }
 
 // returns true if value is in the list; false if not
 bool IntList::contains(int value) const {
-    return false; // REPLACE THIS NON-SOLUTION
+    Node* curr = head;
+    while (curr) {
+        if (curr->info == value)
+            return true;
+        curr = curr->next;
+    }
+    return false;
 }
 
 // returns maximum value in list, or 0 if empty list
 int IntList::max() const {
-    return 0; // REPLACE THIS NON-SOLUTION
+    if (!head) return 0;
+    int maxVal = head->info;
+    Node* curr = head->next;
+    while (curr) {
+        if (curr->info > maxVal)
+            maxVal = curr->info;
+        curr = curr->next;
+    }
+    return maxVal;
 }
 
-// returns average (arithmetic mean) of all values, or
-// 0 if list is empty
+// returns average (arithmetic mean) of all values, or 0 if list is empty
 double IntList::average() const {
-    return 0.0; // REPLACE THIS NON-SOLUTION
+    if (!head) return 0.0;
+    int total = 0, cnt = 0;
+    Node* curr = head;
+    while (curr) {
+        total += curr->info;
+        cnt++;
+        curr = curr->next;
+    }
+    return static_cast<double>(total) / cnt;
 }
 
 // inserts value as new node at beginning of list
 void IntList::push_front(int value) {
-    // IMPLEMENT
+    Node* newNode = new Node;
+    newNode->info = value;
+    newNode->next = head;
+    head = newNode;
 }
 
 // append value at end of list
 void IntList::push_back(int value) {
-    // IMPLEMENT
- 
+    Node* newNode = new Node;
+    newNode->info = value;
+    newNode->next = nullptr;
+    if (!head) {
+        head = newNode;
+        return;
+    }
+    Node* curr = head;
+    while (curr->next) {
+        curr = curr->next;
+    }
+    curr->next = newNode;
 }
 
 // return count of values
 int IntList::count() const {
-   //IMPLEMENT THIS
-   return 0;
+    int cnt = 0;
+    Node* curr = head;
+    while (curr) {
+        cnt++;
+        curr = curr->next;
+    }
+    return cnt;
 }
 
+// Assignment operator should copy the list from the source
+// to this list, deleting/replacing any existing nodes
+IntList& IntList::operator=(const IntList& source) {
+    if (this == &source) return *this;
 
-//Assignment operator should copy the list from the source
-//to this list, deleting/replacing any existing nodes
-IntList& IntList::operator=(const IntList& source){
-    //IMPLEMENT
+    // Delete existing nodes
+    Node* curr = head;
+    while (curr) {
+        Node* next = curr->next;
+        delete curr;
+        curr = next;
+    }
+    head = nullptr;
+
+    // Copy nodes from source
+    curr = source.head;
+    while (curr) {
+        push_back(curr->info);
+        curr = curr->next;
+    }
+
     return *this;
 }
-
-// constructor sets up empty list
-IntList::IntList(){ 
-    //IMPLEMENT THIS 
-}
-
-
-// DO NOT CHANGE ANYTHING BELOW (READ IT THOUGH)
-
-// print values enclose in [], separated by spaces
-void IntList::print() const {
-    Node *n = head;
-    cout << '[';
-    while (n) {
-        cout << n->info;
-        if (n->next)
-            cout << " ";
-        n = n->next;
-    }
-    cout << ']';
-}
-
